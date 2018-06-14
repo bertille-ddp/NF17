b8b320c18611a5e10de7dc10488eb6104895d2e7
ggsab <nf17p054@smestaens24ab.sme.utc>
2018-06-14 13:06:25 +0200

    ajout du fichier drop_all.sql afin de nettoyer sa base de donnée

---
+++ sql/drop_all.sql
@@ -0,0 +1,44 @@
+drop table Ligne;
+/
+drop table Gare;
+/
+drop table Planning;
+/
+drop table Reservation;
+/
+drop table Trajet;
+/
+drop table TypeTrain;
+/
+drop table Ville;
+/
+drop table Voyageur;
+/
+drop table Exceptions;
+/
+drop table Billet;
+/
+drop type typTypeTrain FORCE;
+/
+drop type typTrajet FORCE;
+/
+drop type typException FORCE;
+/
+drop type typPlanning FORCE;
+/
+drop type typLigne FORCE;
+/
+drop type typGare FORCE;
+/
+drop type listeHotels FORCE;
+/
+drop type Hotel FORCE;
+/
+drop type typAdresse FORCE;
+/
+drop type typReservation FORCE;
+/
+drop type typVoyageur FORCE;
+/
+drop type typVille FORCE;
+/
